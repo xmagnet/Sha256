d4d1382e68890e74960b83e7fc6ccfc166d19eb7
sogit <miragent@gmail.com>
2018-10-06 16:12:41 +0900

    Initial commit

---
+++ Unit1.pas
@@ -0,0 +1,39 @@
+unit Unit1;
+
+interface
+
+uses
+  Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes, Vcl.Graphics,
+  Vcl.Controls, Vcl.Forms, Vcl.Dialogs, Vcl.ComCtrls, Vcl.ToolWin, Vcl.ExtCtrls,
+  System.Win.TaskbarCore, Vcl.Taskbar, Vcl.ShellAnimations,
+  System.Hash;// User Added
+
+type
+  TForm1 = class(TForm)
+    procedure ToolButton1Click(Sender: TObject);
+  private
+    { Private declarations }
+  public
+    { Public declarations }
+  end;
+
+var
+  Form1: TForm1;
+
+implementation
+
+{$R *.dfm}
+
+procedure TForm1.ToolButton1Click(Sender: TObject);
+var astr: string;
+    ahash: THashSHA2;
+begin
+ astr:='한글이 이상해 ㅡㅡ;';
+ ahash:=ahash.Create(sha256);
+ caption:=ahash.GetHashString(astr)+'--->HashSize:'+inttostr(ahash.GetHashSize)+' bytes';
+ showmessage('String representation '+IntToStr(Length(ahash.GetHashString(astr)))+' chars');
+ //LenghthOf();
+
+end;
+
+end.
